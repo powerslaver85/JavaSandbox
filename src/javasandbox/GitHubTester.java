b23e3edc371ce6a4c5dcbd6d5b8d67b1170ec6d4
RSCHENK <RSCHENK@AWIPC-RSC01.bandgenossenschaft.ch>
2015-09-28 07:42:05 +0200

    GitHubTest Added

---
+++ src/javasandbox/GitHubTester.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasandbox;
+
+/**
+ *
+ * @author RSCHENK
+ */
+public class GitHubTester {
+    public static void main (String[] args) {
+    System.out.println("Ich bin gepusht, falls  nicht Fail!");
+    }
+   }
